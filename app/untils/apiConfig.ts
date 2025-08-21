918a73df9c03bee0a1b34e91771dfebdcb35dba7
2003thanhquy <quythanhhuynh2003@gmail.com>
2025-08-21 17:25:03 +0700

    url

--- app/untils/apiConfig.ts
+++ app/untils/apiConfig.ts
@@ -1,6 +1,6 @@
 // src/config/apiConfig.ts
 
-const API_BASE_URL = 'https://api-modernlibrary.click';
+const API_BASE_URL = 'https://libmanagefe.onrender.com';
 //const API_BASE_URL = 'http://localhost:8009';
 
 export { API_BASE_URL };
